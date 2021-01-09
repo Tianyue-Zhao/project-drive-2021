f030f216a1c5904174a5ad0d957afdc331e573e7
MaxGh-1C <31356167+MxGh-1C@users.noreply.github.com>
2021-01-09 12:46:47 -0800

    added memory to col_detect also added a trigger if car is stopped for a certain number of data points

--- scripts/train.py
+++ scripts/train.py
@@ -19,6 +19,8 @@ class State:
         self.angular_vel = 0.0
         self.crash_det = False
         self.lap_finish = False
+        self.col_counter = 0
+        self.v_counter = 0
 
 #collision detection function
 #returns true if it determines the car has crashed
@@ -26,17 +28,35 @@ class State:
 def col_detect(state):
     dist_threshold = 0.2    #this is the threshold to determine if an object is
                             #so close as to hit the agent
+    v_threshold = 0.01      #this is the threshold to determine if the agent is
+                            #"stopped" or not
+    found = False
     #this loop iterates over each point in the LaserScan and finds the distance
-    #if the distance is smaller than the threshold it announces a crash
+    #if the distance is smaller than the threshold it updates the found variable
     for point in state.cur_points:
         x = point[0]
         y = point[1]
         dist = math.sqrt(x**2 + y**2)
         if dist < dist_threshold :
-            state.crash_det = True
-            return True
+            found = True
     #if none of the points are too close, no crash detected
-    return False
+    if found :
+        state.col_counter += 1
+    else:
+        #if no crash detected, reset the counter
+        state.col_counter = 0
+
+    #if the agent is "stopped" increment counter, else reset it
+    if state.velocity < v_threshold :
+        state.v_counter += 1
+    else:
+        state.v_counter = 0
+
+    #This variable determines how many crash values must be found before a crash is declared
+    mem_len = 5
+    state.crash_det = ((state.col_counter > mem_len) or (state.v_counter > mem_len))
+
+    return state.crash_det
 
 
 
