f89d141b24e36a1b3aa0cbbf7baaad88120405a3
Tianyue-Zhao <zhaotianyue@outlook.com>
2021-08-05 07:28:21 -0700

    Added names to network layers for transfer learning

--- scripts/network.py
+++ scripts/network.py
@@ -4,17 +4,17 @@ from tensorflow import keras
 def custom_network():
     odom_input = keras.Input(shape=(5,), name="odom", dtype=float)
     laser_input = keras.Input(shape=(1080,), name="laser_scan", dtype=float)
-    D1 = keras.layers.Dense(5, activation="relu")(odom_input)
-    D2 = keras.layers.Dense(5, activation="relu")(D1)
+    D1 = keras.layers.Dense(5, activation="relu", name="Odom_Dense_1")(odom_input)
+    D2 = keras.layers.Dense(5, activation="relu", name="Odom_Dense_2")(D1)
     PC = keras.layers.Reshape((1080,1))(laser_input)
     #Expected output (None, 104, 10)
     #10 Filters, each with kernel size 50, stride 10
-    C1 = keras.layers.Conv1D(10, 50, strides=10, activation="relu")(PC)
+    C1 = keras.layers.Conv1D(10, 50, strides=10, activation="relu", name="Laser_Conv_1")(PC)
     #Add trainable specification for testing
     #C1.trainable = False
-    D3 = keras.layers.Dense(50, activation="relu")(C1)
+    D3 = keras.layers.Dense(50, activation="relu", name="Laser_Dense_1")(C1)
     D3F = keras.layers.Flatten()(D3)
     Combined = keras.layers.Concatenate()([D3F, D2])
-    D4 = keras.layers.Dense(20, activation="relu")(Combined)
+    D4 = keras.layers.Dense(20, activation="relu", name="Combined_Dense_1")(Combined)
     network = keras.Model([odom_input, laser_input], D4)
     return network
